54108cd35b3d959548158cd7410fdc11966a72de
SalimTerryLi <lihangfan@espressif.com>
2021-10-02 16:54:32 +0800

    source tree: basic structure

---
+++ main/main.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2021 SalimTerryLi <lhf2613@gmail.com>
+ * SPDX-FileType: SOURCE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+int app_main(void){
+    return 0;
+}
